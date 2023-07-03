909a09e6eea723f3a83431f93c8c9c6e6377f0bd
talha8571 <talha8571@gmail.com>
2023-07-03 17:34:13 +0500

    codes broken

---
+++ rts/Practice/brokenLinksWithStatusCode.py
@@ -0,0 +1,48 @@
+import time
+import requests
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+class BrokenLinksTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+
+
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        # cls.driver.get("https://www.hulhub.com/")
+        # cls.driver.implicitly_wait(20)
+        cls.driver.get("https://staging.beelinks.solutions/home")
+        cls.driver.find_element_by_xpath("/html/body/app-root/div[2]/app-login/div[1]/div/form/div[1]/input").send_keys("talhahhulhub@gmail.com")
+        cls.driver.find_element_by_xpath("/html/body/app-root/div[2]/app-login/div[1]/div/form/div[2]/input").send_keys("12345678")
+        cls.driver.find_element_by_xpath("/html/body/app-root/div[2]/app-login/div[1]/div/form/button").click()
+
+        cls.driver.maximize_window()
+        cls.driver.implicitly_wait(20)
+
+    def test_broken_links(self):
+        time.sleep(6)
+        # Find all <a> tags in the page
+        links = self.driver.find_elements_by_tag_name("a")
+        # Extract the href attribute from each link
+        link_urls = [link.get_attribute("href") for link in links]
+
+        # Check for broken links
+        for link_url in link_urls:
+            if link_url and link_url.startswith(("http://", "https://")):
+                response = requests.get(link_url)
+                print(f"URL: {link_url} - Status Code: {response.status_code}")
+
+
+            elif link_url:
+                print(f"Ignored URL: {link_url}")
+            else:
+                print("Invalid URL: None")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
